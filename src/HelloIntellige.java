cf47a0b534f3833bba146fc4c5b944a8904ce025
DavPoghosyan <shakilmehedy@gmail.com>
2024-11-05 13:26:02 +0600

    Add input

--- src/HelloIntellige.java
+++ src/HelloIntellige.java
@@ -5,6 +5,7 @@ public class HelloIntellige {
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi again this is Mehedy:");
+        System.out.println("Hi NSW this is Mehedy:");
         String message = scanner.nextLine();
     }
 }
