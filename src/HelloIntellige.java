8490d98319224043b0bf570fb8fe7ace7546cf7c
DavPoghosyan <shakilmehedy@gmail.com>
2024-11-05 13:13:24 +0600

    Add input parameter m

--- src/HelloIntellige.java
+++ src/HelloIntellige.java
@@ -4,7 +4,7 @@ public class HelloIntellige {
 
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi this is Mehedy:");
+        System.out.println("Hi again this is Mehedy:");
         String message = scanner.nextLine();
     }
 }
